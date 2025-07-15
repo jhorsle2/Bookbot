ef9b67fab7355bbf7d6b2fd745f5731e271a4170
jhorsle2 <horsleyjoshua2@gmail.com>
2025-07-15 11:10:42 -0400

    Finsihed next updates to bookbot

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
- print("greetings boots")
+import os
+
+# Set working directory to your project folder
+os.chdir("C:/Python_Projects/Bookbot")
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+    with open(path, encoding='utf-8') as f:
+        return f.read()
+
+main()
