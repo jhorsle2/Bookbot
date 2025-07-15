aa4129c03bf0cd9603ad9378d55ddadd5ed21a40
jhorsle2 <horsleyjoshua2@gmail.com>
2025-07-15 14:25:32 -0400

    Added word counter

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import os
 
-# Set working directory to your project folder
-os.chdir("C:/Python_Projects/Bookbot")
+os.chdir(r'C:\Python_Projects\Bookbot')
+
+
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = 'books/frankenstein.txt'
     text = get_book_text(book_path)
-    print(text)
+    words = word_count(text)
+    print(words)
+
 
 def get_book_text(path):
-    with open(path, encoding='utf-8') as f:
+    with open(path, encoding="utf-8") as f:
         return f.read()
 
+
+def word_count(book_contents):
+    counter = 0
+    word = book_contents.split()
+    for i in word:
+        counter += 1
+    return counter
+
+
+
 main()
+
